7582a1372e906290d48bd437b704313151d5ec5f
GabrielNP96 <gabrielnp1996@gmail.com>
2024-06-17 04:41:51 -0300

    tudo funcionando

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
-const userCep = document.querySelector('#input__cep')
 const answerField = document.querySelector('.answer');
 const btn = document.querySelector('.btn')
 
-async function ConsultCep(cep) {
+async function ConsultCep() {
     try {
-        const searchCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        const userCep = document.querySelector('#input__cep').value;
+        const searchCep = await fetch(`https://viacep.com.br/ws/${userCep}/json/`)
         const cepInfo = await searchCep.json()
-
-        answerField.innerHTML = 
+        const addresHTML = 
         `
             <p>Cep: ${cepInfo.cep}</p>
             <p>Estado: ${cepInfo.uf}</p> 
             <p>Localidade: ${cepInfo.localidade}</p>
             <p>logradouro: ${cepInfo.logradouro}</p>
         `
+
+        answerField.innerHTML = addresHTML
     } catch(error) {
-        answerField.innerHTML += `<p class='error-mensage'>Ouve um erro ao consular o cep, tente novamente.</p>`
+        answerField.innerHTML = `<p class='error-mensage'>Ouve um erro ao consular o cep, tente novamente.</p>`
     }
 }
 
+btn.addEventListener('click', (e) => {
 
-ConsultCep("01001000")
+    e.preventDefault()
+    ConsultCep()
+})
 
 
